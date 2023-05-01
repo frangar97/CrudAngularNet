12f94445c2c6bbdc4449354098afb2c0db5a2e0b
Francisco <frangarcia38@hotmail.com>
2023-05-01 16:50:08 -0600

    Implementation of REST endpoints for EmployeeController

--- BackEnd/API/Controllers/EmployeeController.cs
+++ BackEnd/API/Controllers/EmployeeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Core.Features.Employee;
+using Core.Features.Employee.DTO;
+using Core.Features.Employee.Service;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -7,5 +9,42 @@ namespace API.Controllers
     [ApiController]
     public class EmployeeController : ControllerBase
     {
+        private readonly IEmployeeService employeeService;
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            this.employeeService = employeeService;
+        }
+
+        [HttpGet]
+        public IActionResult GetEmployees()
+        {
+            var employees = employeeService.GetAll();
+            var employeesDTO = employees.Select(x => new EmployeeDTO { Id=x.Id,Name=x.Name,LastName=x.LastName,CreatedDate=x.CreatedDate}).ToList();
+            return Ok(employeesDTO);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateEmployee([FromBody] CreateEmployeeDTO createEmployeeDTO)
+        {
+            var employeeEntity = new EmployeeEntity { Name= createEmployeeDTO.Name,LastName= createEmployeeDTO.LastName,CreatedDate=DateTime.Now };
+            await employeeService.CreateEmployee(employeeEntity);
+            var employeeDTO = new EmployeeDTO { Id=employeeEntity.Id,Name=employeeEntity.Name,LastName=employeeEntity.LastName,CreatedDate=employeeEntity.CreatedDate};
+            return Ok(employeeDTO);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateEmployee(int id,[FromBody] CreateEmployeeDTO createEmployeeDTO)
+        {
+            var employeeEntity = new EmployeeEntity { Name = createEmployeeDTO.Name, LastName = createEmployeeDTO.LastName };
+            await employeeService.UpdateEmployee(id, employeeEntity);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            await employeeService.DeleteEmployee(id);
+            return Ok();
+        }
     }
 }
