a9bcb2dd70067fd0ec39295f4d82a1430ab520b4
Francisco <frangarcia38@hotmail.com>
2023-05-01 15:50:23 -0600

    Repository interface implementation and dbcontext and repository registration

--- BackEnd/API/Program.cs
+++ BackEnd/API/Program.cs
@@ -1,3 +1,8 @@
+using Core.Features.Base;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContextPool<BackEndContext>(options => {
+    options.UseNpgsql(builder.Configuration.GetConnectionString("BackEndConnection"));
+});
+
+builder.Services.AddTransient(typeof(IRepository<>),typeof(BaseRepository<>));
 
 var app = builder.Build();
 
---
+++ BackEnd/Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,47 @@
+﻿using Core.Features.Base;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Infrastructure.Repositories
+{
+    public class BaseRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly BackEndContext _context;
+        protected readonly DbSet<T> _entities;
+
+        public BaseRepository(BackEndContext context)
+        {
+            _context = context;
+            _entities = context.Set<T>();
+        }
+        public async Task AddAsync(T entity)
+        {
+            await _entities.AddAsync(entity);
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            T? entity = await FindAsync(id);
+
+            if (entity != null)
+            {
+                _entities.Remove(entity);
+            }
+        }
+
+        public Task<T?> FindAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _entities.AsEnumerable();
+        }
+
+        public void Update(T entity)
+        {
+            _entities.Update(entity);
+        }
+    }
+}
