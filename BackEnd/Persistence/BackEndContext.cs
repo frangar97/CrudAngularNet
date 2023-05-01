892487a788558800330d81bc2b55f6eccb578aee
Francisco <frangarcia38@hotmail.com>
2023-05-01 15:39:16 -0600

    Configuration for DbContext and Entity with api fluent

---
+++ BackEnd/Persistence/BackEndContext.cs
@@ -0,0 +1,21 @@
+﻿using Core.Features.Employee;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Configuration;
+
+namespace Persistence
+{
+    public class BackEndContext:DbContext
+    {
+        public BackEndContext(DbContextOptions<BackEndContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<EmployeeEntity> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+        }
+    }
+}
---
+++ BackEnd/Persistence/Configuration/EmployeeConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Core.Features.Employee;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.Configuration
+{
+    public class EmployeeConfiguration : IEntityTypeConfiguration<EmployeeEntity>
+    {
+        public void Configure(EntityTypeBuilder<EmployeeEntity> builder)
+        {
+            builder.HasKey(e => e.Id);
+
+            builder.Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(e => e.LastName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(e => e.CreatedDate)
+                .IsRequired()
+                .HasDefaultValue(new DateTime());
+        }
+    }
+}
