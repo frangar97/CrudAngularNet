265b8a403e0cc2cd3eeee6a6eb01a0c28feee85f
Francisco <frangarcia38@hotmail.com>
2023-05-05 19:41:26 -0600

    nginx configuration for serving angular front end

--- FrontEnd/src/app/employee/services/employee.service.ts
+++ FrontEnd/src/app/employee/services/employee.service.ts
@@ -8,7 +8,7 @@ import { ICreateEmployee } from '../interface/ICreateEmployee';
   providedIn: 'root'
 })
 export class EmployeeService {
-  APIURL = "http://backend:5092";
+  APIURL = "http://localhost:5092";
 
   constructor(private http: HttpClient) { }
 
